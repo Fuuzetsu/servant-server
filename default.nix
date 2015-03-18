a5163b31361b79c6543729aa8b4072543a9e959a
Julian K. Arni <jkarni@gmail.com>
2015-03-18 14:38:36 +0100

    Use servant-0.3 in default.nix

--- default.nix
+++ default.nix
@@ -4,12 +4,17 @@
     baseNameOf path != ".git" &&
     baseNameOf path != "result" &&
     baseNameOf path != "dist") ./.
-, servant ? import ../servant {}
 }:
+let
+  servant03 = pkgs.fetchgit {
+    url = https://github.com/haskell-servant/servant.git;
+    rev = "refs/heads/servant-0.3";
+    sha256 = "014xxlbiq5x7yhw4hx6xswckj2vwdn29rw77cznfnkld3srgc3r3";
+  };
+  servant = import servant03 { inherit pkgs; };
+in
 pkgs.haskellPackages.buildLocalCabalWithArgs {
   name = "servant-server";
   inherit src;
-  args = {
-      inherit servant;
-  };
+  args = { inherit servant ; };
 }
